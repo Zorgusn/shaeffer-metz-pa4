c63e0c36a41bcfc8cb5048e5ac57b3b88e654d94
Zane Metz <zanem@cox.net>
2023-12-03 21:18:36 -0500

    setup basim stuff

--- pa-04_PartTwo/basim/basim.c
+++ pa-04_PartTwo/basim/basim.c
@@ -1,126 +1,144 @@
 /*----------------------------------------------------------------------------
-PA-04:  Part Two Intro to Enhanced Needham-Schroeder Key-Exchange with TWO-way Authentication
+PA-04:  Part Two Intro to Enhanced Needham-Schroeder Key-Exchange with TWO-way
+Authentication
 
 FILE:   basim.c
 
-Written By: 
+Written By:
      1- Zane Metz
-	 2- Hudson Shaeffer
-Submitted on: 
+         2- Hudson Shaeffer
+Submitted on:
      Insert the date of Submission here
 ----------------------------------------------------------------------------*/
 
 #include <linux/random.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "../myCrypto.h"
 
 // Generate random nonces for Basim
-void  getNonce4Basim( int which , Nonce_t  value )
+void
+getNonce4Basim (int which, Nonce_t value)
 {
-	// Normally we generate random nonces using
-	// RAND_bytes( (unsigned char *) value , NONCELEN  );
-	// However, for grading purpose, we will use fixed values
+  // Normally we generate random nonces using
+  // RAND_bytes( (unsigned char *) value , NONCELEN  );
+  // However, for grading purpose, we will use fixed values
 
-	switch ( which ) 
-	{
-		case 1:		// the first and Only nonce
-			value[0] = 0x66778899 ;
-			break ;
+  switch (which)
+    {
+    case 1: // the first and Only nonce
+      value[0] = 0x66778899;
+      break;
 
-		default:	// Invalid agrument. Must be either 1 or 2
-			fprintf( stderr , "\n\nBasim trying to create an Invalid nonce\n exiting\n\n");
-			exit(-1);
-	}
+    default: // Invalid agrument. Must be either 1 or 2
+      fprintf (stderr,
+               "\n\nBasim trying to create an Invalid nonce\n exiting\n\n");
+      exit (-1);
+    }
 }
 
 //*************************************
 // The Main Loop
 //*************************************
-int main ( int argc , char * argv[] )
+int
+main (int argc, char *argv[])
 {
-    int       fd_A2B , fd_B2A   ;
-    FILE     *log ;
+  int fd_A2B, fd_B2A;
+  FILE *log;
 
-    char *developerName = "REFERENCE Code by ABOUTABL" ;
+  char *developerName = "Code by Zane and Hudson";
 
-    fprintf( stdout , "Starting Basim's     %s\n" , developerName ) ;
+  fprintf (stdout, "Starting Basim's     %s\n", developerName);
 
-    if( argc < 3 )
+  if (argc < 3)
     {
-        printf("\nMissing command-line file descriptors: %s <getFr. Amal> "
-               "<sendTo Amal>\n\n", argv[0]) ;
-        exit(-1) ;
+      printf ("\nMissing command-line file descriptors: %s <getFr. Amal> "
+              "<sendTo Amal>\n\n",
+              argv[0]);
+      exit (-1);
     }
 
-    fd_A2B    = .... ;  // Read from Amal   File Descriptor
-    fd_B2A    = .... ;  // Send to   Amal   File Descriptor
+  fd_A2B = argv[1]; // Read from Amal   File Descriptor
+  fd_B2A = argv[2]; // Send to   Amal   File Descriptor
 
-    log = fopen("basim/logBasim.txt" , "w" );
-    if( ! log )
+  log = fopen ("basim/logBasim.txt", "w");
+  if (!log)
     {
-        fprintf( stderr , "Basim's %s. Could not create log file\n" , developerName ) ;
-        exit(-1) ;
+      fprintf (stderr, "Basim's %s. Could not create log file\n",
+               developerName);
+      exit (-1);
     }
 
-    BANNER( log ) ;
-    fprintf( log , "Starting Basim\n"  ) ;
-    BANNER( log ) ;
+  BANNER (log);
+  fprintf (log, "Starting Basim\n");
+  BANNER (log);
 
-    fprintf( log , "\n<readFr. Amal> FD=%d , <sendTo Amal> FD=%d\n\n" , fd_A2B , fd_B2A );
+  fprintf (log, "\n<readFr. Amal> FD=%d , <sendTo Amal> FD=%d\n\n", fd_A2B,
+           fd_B2A);
 
-    // Get Basim's master keys with the KDC
-    myKey_t   Kb ;    // Basim's master key with the KDC    
+  // Get Basim's master keys with the KDC
+  myKey_t Kb; // Basim's master key with the KDC
 
-    // Use  getKeyFromFile( "basim/basimKey.bin" , .... ) )
-	// On failure, print "\nCould not get Basim's Masker key & IV.\n" to both  stderr and the Log file
-	// and exit(-1)
-	// On success, print "Basim has this Master Ka { key , IV }\n" to the Log file
-	// BIO_dump the Key IV indented 4 spaces to the righ
-    fprintf( log , "\n" );
-	// BIO_dump the IV indented 4 spaces to the righ
+  // Use  getKeyFromFile( "basim/basimKey.bin" , .... ) )
+  // On failure, print "\nCould not get Basim's Masker key & IV.\n" to both
+  // stderr and the Log file and exit(-1) On success, print "Basim has this
+  // Master Ka { key , IV }\n" to the Log file
+  if (getKeyFromFile ("basim/basimKey.bin", &Kb) == 0)
+    {
+      fprintf (log, "\nCould not get Basim's Masker key & IV.\n");
+      fflush (log);
+      fclose (log);
+      exitError ("\nCould not get Basim's Masker key & IV.\n");
+    }
+  fprintf (log, "Basim has this Master Ka { key , IV }\n");
+  // BIO_dump the Key IV indented 4 spaces to the right
+  BIO_dump_indent_fp (log, &Kb.key, 32, 4);
+  fprintf (log, "\n");
+  // BIO_dump the IV indented 4 spaces to the right
+  BIO_dump_indent_fp (log, &Kb.iv, 16, 4);
+  // Get Basim's pre-created Nonces: Nb
+  Nonce_t Nb;
+  getNonce4Basim(1, Nb);
 
-    // Get Basim's pre-created Nonces: Nb
-	Nonce_t   Nb;  
+  // Use getNonce4Basim () to get Basim's 1st and only nonce into Nb
+  fprintf (log, "Basim will use this Nonce:  Nb\n");
+  // BIO_dump Nb indented 4 spaces to the righ
+  BIO_dump_indent_fp (log, Nb, NONCELEN, 4);
+  fprintf (log, "\n");
 
-	// Use getNonce4Basim () to get Basim's 1st and only nonce into Nb
-    fprintf( log , "Basim will use this Nonce:  Nb\n"  ) ;
-	// BIO_dump Nb indented 4 spaces to the righ
-    fprintf( log , "\n" );
+  fflush (log);
 
-    fflush( log ) ;
+  //*************************************
+  // Receive  & Process   Message 3
+  //*************************************
+  // PA-04 Part Two
+  BANNER (log);
+  fprintf (log, "         MSG3 Receive\n");
+  BANNER (log);
 
-    //*************************************
-    // Receive  & Process   Message 3
-    //*************************************
-    // PA-04 Part Two
-    BANNER( log ) ;
-    fprintf( log , "         MSG3 Receive\n");
-    BANNER( log ) ;
+  //*************************************
+  // Construct & Send    Message 4
+  //*************************************
+  // PA-04 Part Two
+  BANNER (log);
+  fprintf (log, "         MSG4 New\n");
+  BANNER (log);
 
-    //*************************************
-    // Construct & Send    Message 4
-    //*************************************
-    // PA-04 Part Two
-    BANNER( log ) ;
-    fprintf( log , "         MSG4 New\n");
-    BANNER( log ) ;
+  //*************************************
+  // Receive   & Process Message 5
+  //*************************************
+  // PA-04 Part Two
+  BANNER (log);
+  fprintf (log, "         MSG5 Receive\n");
+  BANNER (log);
 
-    //*************************************
-    // Receive   & Process Message 5
-    //*************************************
-    // PA-04 Part Two
-    BANNER( log ) ;
-    fprintf( log , "         MSG5 Receive\n");
-    BANNER( log ) ;
+  //*************************************
+  // Final Clean-Up
+  //*************************************
 
-    //*************************************   
-    // Final Clean-Up
-    //*************************************
+  fprintf (log, "\nBasim has terminated normally. Goodbye\n");
+  fclose (log);
 
-    fprintf( log , "\nBasim has terminated normally. Goodbye\n" ) ;
-    fclose( log ) ;  
-
-    return 0 ;
+  return 0;
 }
